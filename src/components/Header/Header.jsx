746891a0ed04e0518827085ab713caf08c799f88
Princelevi98 <dannyscott98@hotmail.com>
2024-01-24 16:08:40 +0000

    create header component and add some styling to it

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,56 @@
+import "./Header.scss";
+import logo from "../../assets/images/every-step.jpeg";
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+
+const Header = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleMouseEnter = () => {
+    setMenu(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMenu(false);
+  };
+
+  return (
+    <header className="header">
+      <Link className="header__logo-link">
+        <img className="header__logo" src={logo} alt="Charity logo" />
+      </Link>
+      <nav className="header__nav">
+        <div className="header__link-container">
+          <NavLink className="header__link" to="#">
+            Login
+          </NavLink>
+        </div>
+        <div></div>
+        <div
+          className="header__link-container"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <NavLink className="header__link" to="#">
+            Menu
+          </NavLink>
+          {menu && (
+            <div className="header__dropdown-menu">
+              <NavLink className="header__link--sub" to="#">
+                Stock
+              </NavLink>
+              <NavLink className="header__link--sub" to="#">
+                Order
+              </NavLink>
+              <NavLink className="header__link--sub" to="#">
+                About
+              </NavLink>
+            </div>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
