702d634ada7e0453319072b0869dd0034669d706
Princelevi98 <dannyscott98@hotmail.com>
2024-02-03 16:51:11 +0000

    add more images for home page in a slide show

--- src/components/HomeContent/HomeContent.jsx
+++ src/components/HomeContent/HomeContent.jsx
@@ -1,16 +1,38 @@
 import "./HomeContent.scss";
-import image from "../../assets/images/charity-image.jpeg";
+import { useState, useEffect } from "react";
+import imageOne from "../../assets/images/charity-image.jpeg";
+import imageTwo from "../../assets/images/bundles.jpeg";
+import imageThree from "../../assets/images/group.jpeg";
+import imageFour from "../../assets/images/soup-kitchen.jpeg";
+
+const images = [imageOne, imageTwo, imageThree, imageFour];
 
 const Content = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <section className="content">
       <section className="content__container">
         <div className="content__image-container">
-          <img
-            className="content__image"
-            src={image}
-            alt="latest charity event"
-          />
+          {images.map((image, index) => (
+            <img
+              className="content__image"
+              key={index}
+              src={image}
+              alt={`Image ${index + 1}`}
+              style={{
+                display: index === currentImageIndex ? "block" : "none",
+              }}
+            />
+          ))}
         </div>
         <article className="content__text-container">
           <p className="content__text">
