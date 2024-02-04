e888dd2a610c38f5981134205579daaec23d9ec0
Princelevi98 <dannyscott98@hotmail.com>
2024-02-04 21:50:44 +0000

    added information into container for about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,16 @@ const About = () => {
     <section className="about">
       <div className="about__container">
         <article className="about__organisation-info">
-          <p></p>
+          <p className="about__text">
+            Here at Every step of the way it is our goal to help single parent
+            families as much as we can during seasonal periods. This is help
+            with clothes, food, toiletries and access to children’s activities .
+            Our team has help consisting of; Furniture findings, food, drink,
+            social worker support, life coach support, holistic support, toys,
+            clothes and directions to the correct services that may be needed.
+            We also have a client base, which involved 1:1 support . A key
+            worker is allocated when registered.
+          </p>
         </article>
         <article className="about__team-container">
           <p></p>
